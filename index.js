526396651845a2ad80f5646bdbda3ab981b034ca
Terence A Gragston <terence.gragston@gmail.com>
2024-11-12 08:25:24 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let body = document.querySelector('body')
+let div = document.createElement('div')
+let div2 = document.createElement('div')
+
+let ol =  document.createElement('ol')
+let ul = document.createElement('ul')
+let li1 = document.createElement('li')
+let li2 = docuent.createElement('li')
+let li3 = document.createElement('li')
+let li4 = document.createElement('li')
+let li5 = document.createElement('li')
+let li6 = document.createElement('li')
+
+body.appendChild(div).appendChild(ol).appendChild(li1)
+ol.appendChild(li2)
+ol.appendChild(li3)
+div.appendChild
+body.appendChild(div2).appendChild(ul).appendChild(li1)
+ul.appendChild(li5)
+ul.appendChild(li6)
